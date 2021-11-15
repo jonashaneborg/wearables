0c2fb02e6420d319407d790de0e80a76c619492e
jonashaneborg <jon.haneborg@gmail.com>
2021-11-15 12:44:11 +0100

    Code refactor

--- Empatica/empatica.py
+++ Empatica/empatica.py
@@ -3,24 +3,28 @@ import csv
 import datetime as time
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-def analyze(filename, plotting=True, printing=False):
+def EDA(filename, plotting=False, printing=False):
     with open(filename) as file:
         reader = csv.reader(file, delimiter=",")
         datalist = list(reader)
-
         datalist = [float(item) for sublist in datalist for item in sublist]
 
-        # Time and date
         timestamp = int(datalist[0])
-        start_time = time.datetime.fromtimestamp(timestamp)
         freq = datalist[1]
-
         datalist = datalist[2:]
-        n = len(datalist)
+        n = len(datalist)       
 
+        # Time
+        start_time = time.datetime.fromtimestamp(timestamp)
         duration = int(n/freq)
         end_time = start_time + time.timedelta(seconds=duration)        
+
+        # Butter lowpass filter
+        cutoff = 0.05
+        order = 1
+        y = butter_lowpass_filter(datalist, cutoff, order, freq)
         
         if printing:
             print(datalist)
@@ -28,13 +32,13 @@ def analyze(filename, plotting=True, printing=False):
         if plotting:
             x = list(range(n))
 
-            y = butter_lowpass_filter(datalist, 0.05, 1, freq)
-            
             plt.plot(x, datalist)
             plt.plot(x, y)
             
             plt.title(str(start_time.date()) + "\n" + str(start_time.time()) + " - " + str(end_time.time()))
             plt.show()
+
+        return y
 
 
 def butter_lowpass_filter(data, cutoff, order, freq):
@@ -44,4 +48,5 @@ def butter_lowpass_filter(data, cutoff, order, freq):
     y = filtfilt(b, a, data)
     return y
 
-analyze("EDA.csv")
+
+y = EDA("Empatica/Data/EDA.csv", plotting=True)
